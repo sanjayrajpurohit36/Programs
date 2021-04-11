6e9428d9a1fd6a7e0bdb5d5fb4d41cb375997f9e
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-04-11 18:54:46 +0530

    function info

---
+++ Js Prog/output.js
@@ -0,0 +1,26 @@
+/* 
+    1. function declaration/ function statement
+    2. function expression
+    3. first class functions
+    4.
+*/
+
+// in many interiew it is asked.
+console.log(1);
+setTimeout(function () {
+  // this code will be handled by browser call stack so it will execute after 1 sec but till then execution will not stop it will go to next stage and will execute console.log() written below.
+  console.log(2);
+}, 1000);
+console.log(3);
+
+// function declaration or function statement
+function abc() {
+  console.log("Hi I'am Troy!!");
+}
+abc();
+// function expression
+// when ever a function is assigned to a variable
+let x = function () {
+  console.log("inside function x");
+};
+x();
