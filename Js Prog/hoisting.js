4d8c70a091d121598b95f15a2673b665e36a5d5e
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-04-17 18:32:34 +0530

    Added more programs asked in recro test.

---
+++ Js Prog/hoisting.js
@@ -0,0 +1,4 @@
+console.log(" x => ", x); // undefined
+console.log("y =>", y); // in node used before initialization // in browser reference error
+var x = 10;
+let y = 10;
---
+++ Js Prog/recrotest.js
@@ -0,0 +1,40 @@
+// const add = (a) => {
+//   return a + a;
+// };
+// const subt = (a) => {
+//   return a - a;
+// };
+// function test(fnAry, a) {
+//   let ans;
+//   ans = fnAry.forEach((func) => {
+//     let check = func(a);
+//     console.log(check);
+//     return check;
+//   });
+//   return ans;
+// }
+
+// console.log(test([add, subt], 20));
+
+// function newBind() {
+//   return function () {
+//     console.log(this.name);
+//   };
+// }
+
+// function test() {
+//   console.log("yes");
+// }
+// let obj = {
+//   name: "Sanjay",
+// };
+// let callFunc = obj.newBind(obj);
+
+// done tested
+// var check = 10;
+// (() => {
+//   var check = 100;
+//   console.log(check);
+//   var check = 20;
+// })();
+// console.log(check);
