5ea6020e14bed8c0dad9ff656dca0638b5f771b6
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-04-27 10:34:05 +0530

    More updated & oprtimized solution for merging overlapping Intervals

--- Python Prog/mergeIntervals.py
+++ Python Prog/mergeIntervals.py
@@ -1,43 +1,21 @@
-#Medium Range Problem
-# def binSearch(startTime, intervals):
-#     first = 0
-#     last= len(intervals)
-#     while(first <= last):
-#         mid= (first + last) //2
-#         if(startTime >= mid[0] and startTime <=mid[1]):
-            
-#             break
-#         if(mid[0] < startTime):
-#             first = mid+1
-#         else 
-#             last = mid-1
+# Program to merge overlapping intervals
+class Solution:
+    def merge(self, intervals):
+        intervals = sorted(intervals)
+        mergedIntervals = []
+        sizeIntervals = len(intervals)
+        count = -1
+        for i in range(0,sizeIntervals):
+            if(len(mergedIntervals) ==0 or intervals[i][0] > mergedIntervals[count][1] ):
+                mergedIntervals.append(intervals[i])
+                count+=1
+            else:
+                
+                if(mergedIntervals[count][1] >= intervals[i][0]):
+                    mergedIntervals[count][1] = max(mergedIntervals[count][1], intervals[i][1])
+        return mergedIntervals
 
-def search(startTime, intervals):
-    i = 0
-    pos = -1
-    sizeIntervals = len(intervals)
-    for i in range(0,sizeIntervals):
-        if(startTime >= intervals[i][0] and startTime <= intervals[i][1]):
-            pos = i
-            break
-    return pos
-
-def mergeIntervals(intervals):
-    intervals = sorted(intervals)
-    mergedIntervals = []
-    for i in intervals:
-        position = search(i[0], intervals)
-        if(position!=-1):
-            start = intervals[position][0]
-            end = {True:intervals[position][1],False: i[1]}[intervals[position][1] > i[1]]
-            if(len(mergedIntervals)):
-                mergedIntervals.pop(position)
-            mergedIntervals.append([start,end])
-        else:
-            mergedIntervals.append(i)
-    print("MergedIntervals", mergedIntervals)
-
-
-# mergeIntervals([[8,10],[1,3],[11,12],[2,6]]) #output [[1,6],[8,10], [11,12]]
+Object = Solution()
+print(Object.merge([[8,10],[1,3],[11,12],[2,6]])) #output [[1,6],[8,10], [11,12]]
 # mergeIntervals([[1,4],[4,5]]) # output [[1,5]]
-mergeIntervals([[6,8],[1,9],[2,4],[4,7]]) # output [[1,9]]
+# mergeIntervals([[6,8],[1,9],[2,4],[4,7]]) # output [[1,9]]
