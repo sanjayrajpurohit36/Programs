bafc9828d8be0a2757ad36a4b5df40c59ca2f55d
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-05-10 22:46:21 +0530

    Concept Program for output.

--- Js Prog/output2.js
+++ Js Prog/output2.js
@@ -1,5 +1,13 @@
+/**
+ * Function test shows that a is not defined and b is defined outside the function because
+ * var a = (b = 3); is interpreted
+ * b = 3
+ * var a = b
+ * so b becomes the global variable and a = b so a is not accessible outisde the function test.
+ */
 let test = function () {
-  var a = (b = 21);
+  var a = (b = 3);
   console.log(a, b);
 };
 test();
+console.log(a, b);
