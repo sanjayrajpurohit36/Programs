9d974233dbba758e5052f40b71cb03be610e1eb2
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-05-11 16:25:31 +0530

    Added output programs for object.

---
+++ Js Prog/output3.js
@@ -0,0 +1,20 @@
+// Output program of js
+// "use strict";
+
+// let name = "Roy";
+// let obj = {
+//   name: "Max",
+//   go: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// let name = "Roy";
+// let obj = {
+//   name: "Max",
+//   go: function () {
+//     console.log(this.name, name);
+//   },
+// };
+
+obj.go();
