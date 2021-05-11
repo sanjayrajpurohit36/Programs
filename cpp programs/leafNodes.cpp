3928241f796f788380731d2739053051319e16b4
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-05-11 16:25:55 +0530

    Added Program to print the leaf nodes of a BST.

---
+++ cpp programs/leafNodes.cpp
@@ -0,0 +1,64 @@
+/**
+ * Print all the leaf nodes of BST
+ * */
+// #include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+} *root = NULL;
+
+void createNode(int n)
+{
+    Node *temp, *itr, *parent;
+    temp = (struct Node *)(calloc(sizeof(struct Node), 1));
+    temp->data = n;
+    temp->left = NULL;
+    temp->right = NULL;
+    if (root == NULL)
+    {
+        root = temp;
+    }
+    else
+    {
+        itr = root;
+        while (itr != NULL)
+        {
+            parent = itr;
+            if (temp->data >= itr->data) // go to right part of tree
+                itr = itr->right;
+            else // temp-> data < itr -> data go to left part ot the tree
+                itr = itr->left;
+        }
+        if (parent->data >= temp->data)
+            parent->left = temp;
+        else
+            parent->right = temp;
+    }
+}
+
+void PreOrder(Node *temp) // Node left right
+{
+    if (temp == NULL)
+        return;
+    if (temp->right == NULL && temp->left == NULL)
+        cout << temp->data << " -> ";
+    PreOrder(temp->left);
+    PreOrder(temp->right);
+}
+
+int main()
+{
+    int arr[] = {0, 1, 2, 4, 5, 7, 9};
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        createNode(arr[i]);
+    }
+    PreOrder(root);
+    return 0;
+}
