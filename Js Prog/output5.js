976952e92260e275014cf251fd0d1c2aa28f846f
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-06-08 00:42:40 +0530

    Added comments in output programs.

--- Js Prog/output5.js
+++ Js Prog/output5.js
@@ -11,12 +11,16 @@ function local() {
 function let() {
   if (true) {
     let a = 4;
+    /* this let is shadowing the a but only in this
+       block inside the block the value of a
+       will be 4 but outside the block it will be 3
+    */
   }
   console.log("It will refer to global var coz let uses block scoping:", a); //3
 }
 function test() {
   if (true) {
-    x = 4;
+    x = 4; // this line of code is chaning the global x to 4
   }
   console.log("In test", x); //4
 }
