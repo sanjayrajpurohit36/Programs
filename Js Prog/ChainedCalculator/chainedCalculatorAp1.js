4ad347d466ef4bd7746b711753366dde8daa31f2
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-06-07 11:40:20 +0530

    Added 2 approaches of chained calc.

---
+++ Js Prog/ChainedCalculator/chainedCalculatorAp1.js
@@ -0,0 +1,28 @@
+/*  chained calculator approach 2 
+    In this approach we will use the this object & will add all the functions to that object
+    the drawback of this approach is only that we're pollutint the global this object of the JS.
+*/
+function calc(initialValue = 2) {
+  // accumulator
+  var result = initialValue;
+  console.log("checking if global is equals to this", global == this);
+  //to prove that we're polluting the global this object,this will return true
+  this.add = function (a) {
+    //Note:- here we can create normal functions as well
+    result += a;
+    return this;
+  };
+  this.subt = function (a) {
+    result -= a;
+    return this;
+  };
+  this.res = function () {
+    return result;
+  };
+  return this;
+}
+
+console.log(
+  "result of chained calc is: =>",
+  calc(2).add(2).add(2).subt(6).res()
+);
