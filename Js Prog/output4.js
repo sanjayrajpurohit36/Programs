988279c1513e3fe831363e09b8adf2675bc95ebe
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-06-12 13:25:07 +0530

    Minor changes.

--- Js Prog/output4.js
+++ Js Prog/output4.js
@@ -1,13 +1,13 @@
-/* 
+/*
  Global excution context will be created and x = undefined will be set in memory allocation phase
  then in II phase x ki value 100 in code execution phase so 100 will be printed a line no 12.
- 
- EC of annonymus function will be created and x will be set as undefined in memory execution phase 
+
+ EC of annonymus function will be created and x will be set as undefined in memory execution phase
  then in II phase x = 10; will be set in code execution phase.
  then console.log(x) will print 10 at line no 17.
-    
+
  then EC of annonymus function will be deleted;
- 
+
  then in GEC x is still present then console.log(x) at line 19 will pring 100.
 */
 var x = 100;
--- Python Prog/testr.py
+++
@@ -1,3 +0,0 @@
-a,b = 3,6
-c = b/a
-print(c,type(c))
