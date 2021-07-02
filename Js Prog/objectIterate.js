0d96e3d8696a70f43941bc4acb17af3e26132048
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-02 17:41:17 +0530

    Iterating an object to get keys.

---
+++ Js Prog/objectIterate.js
@@ -0,0 +1,11 @@
+const person = { name: "Sanjay", surname: "Raj" };
+for (const key in person) {
+  console.log(key);
+}
+
+console.log(typeof typeof null);
+/* 
+typeof typeof null
+typeof Object
+ans: string
+*/
