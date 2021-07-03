0dd1218dfc09b9363604a4e97f032827033d167b
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-03 17:03:50 +0530

    Added a shorthand code for swapping two values.

---
+++ Js Prog/swapShorthand.js
@@ -0,0 +1,11 @@
+// Short Hand
+let even = 2;
+let odd = 3;
+[odd, even] = [even, odd];
+
+console.log("swapped values", odd, even);
+
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+console.log(arr);
