3760ad09eeba037058edb9fc549f856c9d78feb5
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-06 08:32:29 +0530

    Added program for cacl of a div with / without border box property.

--- Js Prog/output10.js
+++ Js Prog/output10.js
@@ -2,3 +2,13 @@ function test() {
   return 0;
 }
 console.log(typeof test()); // number
+
+function test() {
+  // return 0;
+}
+console.log(typeof test()); // undefined
+
+function test() {
+  return 0;
+}
+console.log(typeof test); // object
