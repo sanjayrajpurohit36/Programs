765688e091135fabfcf541b604cfb73aa31df3cb
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-09 23:58:05 +0530

    code comment changes.

--- Js Prog/polyfillforEach.js
+++ Js Prog/polyfillforEach.js
@@ -4,7 +4,7 @@
 
 Array.prototype.myForEach = function (callback) {
   if (typeof callback != "function") {
-    throw new Error("Expected a function as param got something else");
+    throw new Error("Expected a function in argument got something else");
   } else {
     for (let i = 0; i < this.length; i++) {
       callback(this[i]);
