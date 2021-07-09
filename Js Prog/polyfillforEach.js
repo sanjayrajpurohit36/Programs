37a22b86530c168ef3d4d125a082bdf9514570bc
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-09 23:52:42 +0530

    WAP a program for Polyfill for forEach in JS.

---
+++ Js Prog/polyfillforEach.js
@@ -0,0 +1,17 @@
+/*
+    Write a polyfill for forEach in JS
+*/
+
+Array.prototype.myForEach = function (callback) {
+  if (typeof callback != "function") {
+    throw new Error("Expected a function as param got something else");
+  } else {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i]);
+    }
+  }
+};
+
+[1, 2, 3].myForEach((value) => {
+  console.log(value);
+});
