af52784f0405113043213944910b4cd5523c5012
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-12 12:01:52 +0530

    Added Info about Memory Leak in JS.

---
+++ Js Prog/MemoryLeak/scopeLookup.js
@@ -0,0 +1,21 @@
+/*
+    Scope lookup happens when we create global variables means we reserve memory in 
+    global space / global object / window object that memory is not garbage collected by the garbage collector.
+*/
+
+function someFunc() {
+  b = 10;
+}
+
+// is the same as
+
+function someFunc() {
+  window.b = 10; // Oops!
+
+  // This happens due to scope lookup
+  // If the variable does not exist in any scope
+  // the JS runtime creates the variable
+  // in the GLOBAL scope! 😱
+}
+
+// The variable is never cleaned
