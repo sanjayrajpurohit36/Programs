43e9b4119732c1922d0c1e235ca9e690c37f5e58
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-15 13:23:52 +0530

    Added detailed explanation for forEach polyfill and modified the code for rest parmeters.

--- Js Prog/polyfillforEach.js
+++ Js Prog/polyfillforEach.js
@@ -1,5 +1,23 @@
 /*
     Write a polyfill for forEach in JS
+    
+    ForEach accepts 2 params 
+    1. * Callback function with three params (currenValue, index, arr) 
+    2. (Optional) thisValue a value which can be passed to the function to be used as its this value, by default 
+       it is undefined
+    Syntax:
+    
+    forEach(function callback(currentValue, index, arr){
+      console.log(currentValue)
+    }, , thisValue)
+    
+    // OR
+
+    forEach(callback(currentValue, index, arr) => {
+      console.log(currentValue)
+    }, , thisValue)
+    
+    Note: * : Required
 */
 
 Array.prototype.myForEach = function (callback) {
@@ -7,11 +25,16 @@ Array.prototype.myForEach = function (callback) {
     throw new Error("Expected a function in argument got something else");
   } else {
     for (let i = 0; i < this.length; i++) {
-      callback(this[i]);
+      callback(this[i], i, this);
+      /* Here 
+        1. this[i] is the currentValue
+        2. i is the element
+        3. this is the array
+      */
     }
   }
 };
 
-[1, 2, 3].myForEach((value) => {
-  console.log(value);
+[1, 2, 3].myForEach((value, index, array) => {
+  console.log(`value: ${value},  index: ${index}, array: ${array}`);
 });
