aa83735e795d8da4ffdad5091d5c225025c68a2c
Sanjay <sanjay.rajpurohit@embibe.com>
2021-07-24 23:52:50 +0530

    Output program on concept of timeout in JS.

---
+++ Js Prog/InterviewAskedQuestions/output4.js
@@ -0,0 +1,13 @@
+const promise = new Promise((resolve, reject) => {
+  console.log(1);
+  setTimeout(() => {
+    console.log("timerStart");
+    resolve("success");
+    console.log("timerEnd");
+  }, 0);
+  console.log(2);
+});
+promise.then((res) => {
+  console.log(res);
+});
+console.log(4);
