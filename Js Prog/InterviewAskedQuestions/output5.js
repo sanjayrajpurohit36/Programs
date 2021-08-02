a35b1e90eb2decff0365b20b24091178f29fbbe1
Sanjay <sanjay.rajpurohit@embibe.com>
2021-08-02 08:24:24 +0530

    Added Explanation of the output code.

--- Js Prog/InterviewAskedQuestions/output5.js
+++ Js Prog/InterviewAskedQuestions/output5.js
@@ -1,3 +1,8 @@
+/*
+    Output question based on the internals of JS, MTQ, CB, Event Loop.
+    
+    What will be the log sequence of the following code.
+*/
 let resolver;
 const promise = new Promise((resolve) => {
 	resolver = resolve;
@@ -8,10 +13,23 @@ promise.then((res) => {
 });
 
 setTimeout(() => {
-	console.log("1 timeout cb");
-	resolver("Resolved");
+	console.log("1st Timeout cb");
+	resolver("Promise Resolved");
 }, 3000);
 
 setTimeout(() => {
-	console.log("2 timeout cb");
+	console.log("2nd Timeout cb");
 }, 3000);
+
+/*
+    Output: 
+    1st Timeout cb
+    Promise Resolved
+    2nd Timeout cb
+
+
+    Reason: The callbacks of the Promises are kept in the "Micro Task Queue", whereas the other callbacks are 
+    pushed inside the "Callback Queue" also the tasks inside the Micro Task Queue are given priority in execution 
+    that's why after the first callback of setTimeout which will be executed "3sec" the "then" block of the Promise 
+    will be executed then the callback of the second Timeout will be executed
+*/
