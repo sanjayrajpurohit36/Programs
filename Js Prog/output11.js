8efe71ef3f456f02c1e86a38b3407fc25b4151e5
Sanjay <sanjay.rajpurohit@embibe.com>
2021-08-04 19:03:19 +0530

    Added a program based on the concept of hoisting.

---
+++ Js Prog/output11.js
@@ -0,0 +1,31 @@
+/*
+    What will be the output of this function
+    Concept: Hoisting
+*/
+function parent() {
+	var hoisted = "'I’m a variable";
+	function hoisted() {
+		return "I’m a function";
+	}
+	return hoisted();
+}
+// console.log(parent()); // un-comment this and check the output
+
+// Output: Error : hoisted is not a function
+/* 
+    Reason: In the function parent the var hoisted will have the value "I'm a variable"
+    it will not be overwritten by the function hoisted() until we assign 
+    the function hoisted to the variable
+
+*/
+
+// Rectified Code:
+
+function parent() {
+	var hoisted = "'I’m a variable";
+	var hoisted = function hoisted() {
+		return "I’m a function";
+	};
+	return hoisted();
+}
+console.log(parent());
