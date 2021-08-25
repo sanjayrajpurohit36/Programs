95c4254b4cdf4e0b1e15e108c0dc02aa9f77f501
Sanjay <sanjay.rajpurohit@embibe.com>
2021-08-25 21:42:56 +0530

    Added a program for the concept of this of arrow Functions.

---
+++ Js Prog/arrowThis.js
@@ -0,0 +1,18 @@
+/* 
+    What will be the output of this code
+*/
+
+const thisObj = {
+	thisFunc() {
+		const arrowFunc = () => console.log(this);
+		arrowFunc();
+	},
+};
+
+thisObj.thisFunc();
+
+/*
+    As the FAT arrow function don't have their own this that's why they 
+    reference / inherit "this" from their ancestors or parent.
+    Output: {thisFunc : [Function: thisFunc]}
+*/
