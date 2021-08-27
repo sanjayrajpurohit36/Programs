2225ef46a1bff68858b72b5bcef02d83709378eb
Sanjay <sanjay.rajpurohit@embibe.com>
2021-08-27 10:01:42 +0530

    Updated code for concept of Execution Context in JS.

--- Js Prog/EC/ec.js
+++
@@ -1,8 +0,0 @@
-// This is to check 1st phase of EC creation(Execution Context)
-var x = 7; // Add a debugger here and check the scope to check the value of variable in memory allocation phase.
-function getName() {
-  console.log("Hello I am JS");
-}
-
-console.log(x);
-getName();
---
+++ Js Prog/ExecutionContext/index.js
@@ -0,0 +1,8 @@
+// This is to check 1st phase of EC creation(Execution Context)
+// Add a debugger here and check the scope to check the value of variable in memory allocation phase.
+var x = 7;
+function getName() {
+	console.log("Hello I am JS");
+}
+console.log(x);
+getName();
