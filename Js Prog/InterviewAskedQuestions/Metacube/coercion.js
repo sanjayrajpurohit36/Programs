0b8d9c8313d68407124a76ea05fd4920b1db766f
Sanjay <sanjay.rajpurohit@embibe.com>
2021-09-12 23:51:07 +0530

    Added type coercion interview asked program in JS.

---
+++ Js Prog/InterviewAskedQuestions/Metacube/coercion.js
@@ -0,0 +1,9 @@
+/* 
+    Tell the output of these programs and explain them
+*/
+var x = 3;
+var y = "3";
+console.log(x + y); // 33
+console.log(x - y); // 0
+console.log(x * y); // 9
+console.log(x / y); // 1
