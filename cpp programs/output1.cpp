304a62c58e7a5d807bf7dff3817fa86124974f62
Sanjay <sanjay.rajpurohit@embibe.com>
2021-10-07 23:25:18 +0530

    Added a Cpp output program.

---
+++ cpp programs/output1.cpp
@@ -0,0 +1,12 @@
+/*
+    What will be the output of the following code.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    char ch = 'a';
+    ch = ch + a;
+    cout << ch << endl;
+}
