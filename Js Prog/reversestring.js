2683526af85c7a7470191bad2b8f26224b785955
Sanjay Rajpurohit <sanjayrajpurohit1996@gmail.com>
2021-10-17 01:15:09 +0530

    Added Program Info and some test cases.

--- Js Prog/reversestring.js
+++ Js Prog/reversestring.js
@@ -1,21 +1,31 @@
-var str = "a,b$c",
-  arr = str.split(""),
-  length = str.length,
-  startPointer = 0,
-  endPointer = length - 1;
-
-while (startPointer < endPointer) {
-  if (!(arr[startPointer] >= "a" && arr[startPointer] <= "z")) {
-    startPointer += 1;
-  } else if (!(arr[endPointer] >= "a" && arr[endPointer] <= "z")) {
-    endPointer -= 1;
-  } else {
-    let tempChar = arr[startPointer];
-    arr[startPointer] = arr[endPointer];
-    arr[endPointer] = tempChar;
-    startPointer += 1;
-    endPointer -= 1;
+/*
+  Write a program to reverse a string without switching the positions of the special characters in the given input stirng.
+*/
+function reverseString(str) {
+  var arr = str.split(""),
+    length = str.length,
+    startPointer = 0,
+    endPointer = length - 1;
+  
+  while (startPointer < endPointer) {
+    if (!(arr[startPointer] >= "a" && arr[startPointer] <= "z")) {
+      startPointer += 1;
+    } else if (!(arr[endPointer] >= "a" && arr[endPointer] <= "z")) {
+      endPointer -= 1;
+    } else {
+      let tempChar = arr[startPointer];
+      arr[startPointer] = arr[endPointer];
+      arr[endPointer] = tempChar;
+      startPointer += 1;
+      endPointer -= 1;
+    }
   }
+  return arr;
 }
+var inputArray = ["a,b$c", "m@x", "$@mi", "teacher", "!@#$%"]
+var result
+inputArray.forEach((element) => {
+  result = reverseString(element)
+  console.log("reversed arring without changing special chars=>", result.join(""));
+})
 
-console.log("reversed arring without changing special chars=>", arr.join(""));
