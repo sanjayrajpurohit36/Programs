af70e614dab31a45a7d20f79e652fe4df6e63a59
Sanjay Rajpurohit <sanjayrajpurohit1996@gmail.com>
2021-10-17 20:18:22 +0530

    Added a program on operators of c/cpp.

---
+++ Cpp Prog/CodingNinja/Operators/operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+int main() { 
+    double a = 6/4; // 1 because bboth the operands are int
+    int b = 6/4; // 1
+    int c = a+b; // 1+1
+    cout<<c;
+    return 0;
+}
