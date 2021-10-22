b5a781892612adb6be1ae5bb9c9b551824f0914d
Neha <nehakarmakar1721@gmail.com>
2021-10-22 16:56:23 +0530

    Minor changes in program info.

--- Js Prog/defaultValue.js
+++ Js Prog/defaultValue.js
@@ -1,5 +1,5 @@
 // JS Tricks
-// we can use or operator to return the default value of any var.
+// we can use OR operator to return the default value of any var.
 const { log } = console;
 let person = { name: "Max", city: "New York" };
 log(person.age || 23); // => 23 Reason: person.age is undefined so undefined || 23 will return 23
