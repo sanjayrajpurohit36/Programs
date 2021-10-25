940b447baaec3d2d13293f24ffc27c4ad3681028
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-10-25 19:28:04 +0530

    Addedd output program asked in Jio.

---
+++ Js Prog/InterviewAskedQuestions/Jio/output2.js
@@ -0,0 +1,21 @@
+// What will the output of the below code snippet if executed one by one
+/**
+ * Syntax of fill function in Js
+ * fill(value)
+ * fill(value, start)
+ * fill(value, start, end)
+ * 
+ * uncomment the below codes and check the output
+ */
+let abc = [3,6,9,12]
+abc.fill(2,2);
+console.log(abc.toString())
+
+// abc.fill(0,1,3)
+// console.log(abc.toString())
+
+// abc.copyWithin(2,0)
+// console.log(abc.toString())
+
+// abc.copyWithin(2,0,1)
+// console.log(abc.toString())
