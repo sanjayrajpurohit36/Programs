8ea3e43715b1b067ce989d237452c3fc11b0fc31
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-12-01 14:21:10 +0530

    Added a program asked in Flipkart's UI Developer Interview.

---
+++ Js Prog/InterviewAskedQuestions/Flipkart/output1.js
@@ -0,0 +1,16 @@
+/* 
+    What will be the output of the following code
+*/
+
+
+var a = 3
+function ab() {
+  var a = 2
+  setTimeout(() => console.log(this.a))
+}
+c = ab()
+ 
+/*
+    This will give 3 as output as the var a on line no 6 is present in global context not the var a which is inside the function
+    so that's why it will give 3 as output 
+*/
