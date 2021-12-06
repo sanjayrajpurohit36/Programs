fddaf8899106f379166899e0fc60bd166aa67707
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-12-07 01:24:24 +0530

    Updated code comments.

--- Js Prog/arrowThis.js
+++ Js Prog/arrowThis.js
@@ -13,11 +13,12 @@ thisObj.thisFunc();
 console.log(thisObj);
 
 /*
-    As the FAT arrow function don't have their own this that's why they 
+    Key Point: As the FAT arrow function don't have their own "this" that's why they 
     reference / inherit "this" from their ancestors or parent.
+    
     Also, here MIP is used Method Invocation Pattern due to which the this inside the 
     FAT arrow function will refer this as whatever is written before "." operator.
-    so, 
+    so,
     Output: thisObj which is
     {thisFunc : [Function: thisFunc]}
 */
