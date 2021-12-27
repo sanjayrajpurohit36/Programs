0ed4a88cecabdbc6ec019d39a942d045b39fe0f0
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-12-27 20:35:01 +0530

    Changes in Explanation comments.

--- Js Prog/functiondiff.js
+++ Js Prog/functiondiff.js
@@ -5,15 +5,16 @@ function x() {
 }
 
 // arrowFunc(); // this will print arrowFunc is not a function
-/* this will print "this will print arrowFunc is not a function" as arrow functions are not 
+
+/* 
+Reason : this will print "this will print arrowFunc is not a function" as arrow functions are not 
 hoisted only their declaration is hoisted not the complete expression means only var 
-arrowFunction is hoisted it is treated as var.*/
+arrowFunction is hoisted it is treated as a normal variable in Js.*/
 var arrowFunc = () => {
   console.log("yes with arrow function");
 };
 
 // This concept
-
 const testObject = {
   testFunc: () => {
     return this;
@@ -21,10 +22,15 @@ const testObject = {
 };
 
 const testFuncCall = testObject.testFunc;
-console.log(testFuncCall()); // it will return the object as this will refer to the parent
+console.log(testFuncCall()); 
+/* 
+  it will return an empty object as FAT arrow functions don't have their own this as this will 
+  refer to the parent for their "this"
+*/
 
-/*  Key points
+
+/*  Key points:-
 1. Arrow function are not hoisted
 2. `this` keyword in arrow function is basically what its value would be if 
-    function is not there
+    function is not there.
 */
