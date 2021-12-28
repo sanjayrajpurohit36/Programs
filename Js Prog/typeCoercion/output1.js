624f6f78a60fc33ee398b51b244e472936cf2a18
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-12-28 22:01:02 +0530

    Added explanation.

--- Js Prog/typeCoercion/output1.js
+++ Js Prog/typeCoercion/output1.js
@@ -18,6 +18,11 @@ console.log("Yay it's a string!")
 /*
 
 Explanation: 
+randomValue will be 23
+23 is of type "number"
+so, typeof randomValue will be "number" but the !before the typeof will change the number to (boolean)false 
+so when false === "string" will compared it will be false, hence the if condition will be false so, 
+it will go in else block and as a result of that "Yay it's a string!" will be printed.
 
 
 
