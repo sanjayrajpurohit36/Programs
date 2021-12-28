9dae53d7ebc7d0edaa328521e4b807d01a50078a
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2021-12-28 21:42:27 +0530

    Added output program on type coercion.

---
+++ Js Prog/typeCoercion/output1.js
@@ -0,0 +1,24 @@
+/*
+    What will be the output of this
+
+*/
+
+let randomValue = { name: "Lydia" }
+randomValue = 23
+console.log(!(typeof randomValue === "string"))
+
+if (!typeof randomValue === "string") {
+    console.log("It's not a string!")
+} else {
+console.log("Yay it's a string!")
+}
+
+// Output: Yay it's a string!
+
+/*
+
+Explanation: 
+
+
+
+*/
