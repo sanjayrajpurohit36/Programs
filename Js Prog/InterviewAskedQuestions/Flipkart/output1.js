e7224385ad6d67afe4fd5b92e516b1311f48868f
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-01-12 21:16:31 +0530

    Added output code in Js, on the concept of execution context.

--- Js Prog/InterviewAskedQuestions/Flipkart/output1.js
+++ Js Prog/InterviewAskedQuestions/Flipkart/output1.js
@@ -14,3 +14,14 @@ c = ab()
     This will give 3 as output as the var a on line no 6 is present in global context not the var a which is inside the function
     so that's why it will give 3 as output 
 */
+
+
+let x = 3
+function check() {
+  console.log(this.a);  // undefined
+}
+check()
+
+/*
+  Reason: as let & const do not get declared in the same context
+*/
