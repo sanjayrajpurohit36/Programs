cc77dcdfbbfa6fa0c213b666e0960c0f91edd4bc
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-01-28 01:07:39 +0530

    Added Polyfill for String.prototype.split() function.

--- Js Prog/polyfillSplit.js
+++ Js Prog/polyfillSplit.js
@@ -9,9 +9,48 @@ String.prototype.acutalSplit = String.prototype.split;
 // Storing original split function in String's prototype so that we can use it while creating modified polyfill
 
 // Polyfill
-String.prototype.split = function (delimiter = "$") {
-	// this = str as MIP (Method Invocation pattern is used)
-	return this.acutalSplit(delimiter);
+String.prototype.split = function (pattern = "$") {
+        let ans = [];
+        if(!pattern || pattern.length > this.length  ) 
+          ans.push(this);
+        else if(pattern.length == 0)
+          ans.push(...this)
+        else {
+          let tempStr = '';
+          let comparedStr = ''
+          for(let i = 0,j = 0; i < this.length ; ) {
+            
+            if(this[i] === pattern[j]) {
+              let k = 1;
+              let isMatched = true;
+              
+              comparedStr += this[i++]; // setting the already matched char in the comparedStr and skipping that matched character to reduce one extra comparison
+              j++; // skipping the already matched char of pattern 
+      
+              for(k ; k < pattern.length && k < this.length; k++) {
+                comparedStr += this[i];
+                if(str[i++] !== pattern[j++]) {
+                  isMatched = false;
+                  break;
+                }
+              }
+              j = 0; // if the pattern is matched or not we need to start the comparison for the pattern again from 0th index.
+              if(isMatched) {
+                ans.push(tempStr); // pushing all the chars before the pattern into the ans array 
+                tempStr = ''
+                comparedStr = ''
+              } else {
+                tempStr += comparedStr;
+                comparedStr = '';
+              }
+            } else {
+                tempStr +=this[i];
+                i++;
+            }
+          } 
+          ans.push(tempStr);
+        }
+        return ans;
 };
 
-str.split();
+console.log(str.split());
