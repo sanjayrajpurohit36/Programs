164f8f2d20b7c106c177b905c9c811e062938aca
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-02-04 00:26:16 +0530

    Updated prog name.

--- Cpp Prog/CodingNinja/Recursion/FindFirstIndex.cpp
+++ Cpp Prog/CodingNinja/Recursion/FindFirstIndex.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 bool checkNumber(int input[], int size, int x) {
-    if(size == 0) return false; // if size of arr is 0 means element doesn't exist in array as the array itself doesn't exist
-    else if(input[0] == x) return true;
+    if(input[0] == x) return true;
     else return checkNumber(input + 1, size - 1, x);
 }
 
