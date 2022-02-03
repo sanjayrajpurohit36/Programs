690ca4ef051e0bc970344d0ae0d48b5734b94259
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-02-03 16:41:46 +0530

    WAP to check the exixtance of a given number in array using recursion.

---
+++ Cpp Prog/CodingNinja/Recursion/FindNumberInArray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+bool checkNumber(int input[], int size, int x) {
+    if(size == 0) return false; // if size of arr is 0 means element doesn't exist in array as the array itself doesn't exist
+    else if(input[0] == x) return true;
+    else return checkNumber(input + 1, size - 1, x);
+}
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: \n";
+    cin >> n;
+  
+    int *input = new int[n];
+    cout << "Enter the the array: \n";
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    cout << "Enter the number to check it's existance in the array: \n";
+    cin >> x;
+    
+    if(checkNumber(input, n, x)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+}
