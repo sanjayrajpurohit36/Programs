e898ba007eafab92dfa15b55a0f84f71ca9f22b1
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-02-10 01:02:10 +0530

    Added new program for understanding Js new Array function.

---
+++ Js Prog/output25.js
@@ -0,0 +1,36 @@
+/**
+ * What will be the output of the below written Js code
+ * 
+ */
+
+let arr = new Array([1, 2, 4, 4, 5])
+let arr2 = new Array();
+let arr3 = new Array([])
+
+
+console.log(arr)
+console.log(arr2)
+console.log(arr3)
+
+console.log(arr.length)
+console.log(arr2.length)
+console.log(arr3.length)
+
+
+/**
+ * Ans : [ [ 1, 2, 4, 4, 5 ] ]
+ * []
+ * [ [] ]
+ * 1
+ * 0
+ * 1
+ * 
+ * arr will be having one element which will be an array of elements
+ * arr 2 will be an empty array
+ * arr 3 will have one element which will be an empty array
+ * 
+ * So the size of the arrays will be
+ * 1
+ * 0
+ * 1
+ */
