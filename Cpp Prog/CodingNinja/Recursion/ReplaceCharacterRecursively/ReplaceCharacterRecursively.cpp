7a192dbc64f801020f1eb28f4f96e2389f152558
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-02-23 10:50:58 +0530

    Program description update.

--- Cpp Prog/CodingNinja/Recursion/ReplaceCharacterRecursively/ReplaceCharacterRecursively.cpp
+++ Cpp Prog/CodingNinja/Recursion/ReplaceCharacterRecursively/ReplaceCharacterRecursively.cpp
@@ -1,6 +1,6 @@
 /*
-    WAP to remove all the given char present in a char array using recursion.
-
+    Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+    Do this recursively.
 */
 #include<iostream>
 using namespace std;
