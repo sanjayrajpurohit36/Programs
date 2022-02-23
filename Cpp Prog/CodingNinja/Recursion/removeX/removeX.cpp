bc2e946be499cc3cd03e8cc1b9bcbca17bd7a95b
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-02-23 10:35:11 +0530

    New method to remove x from given string.
    Added inpout and output files as well for reference.

--- Cpp Prog/CodingNinja/Recursion/removeX/removeX.cpp
+++ Cpp Prog/CodingNinja/Recursion/removeX/removeX.cpp
@@ -5,6 +5,7 @@
 #include<iostream>
 using namespace std;
 
+// Method 1
 void removeXHelper(char input[], int index = 0, int xIndex = 0) {
     if(input[index] == '\0') { 
         input[index-xIndex] = '\0';
@@ -19,14 +20,41 @@ void removeXHelper(char input[], int index = 0, int xIndex = 0) {
     }
 }
 
-void removeX(char input[]) {
+void removeXM1(char input[]) {
     removeXHelper(input);
+}
+//
+
+
+// Method 2
+void removeXM2(char *str) {
+    if(str[0] == '\0') {
+        return ;
+    } else if(str[0] == 'x') {
+        int i = 0;
+        for(; str[i]!='\0'; i++) {
+            str[i] = str[i+1];
+        }
+        str[i] = str[i+1];
+        removeXM2(str);
+    } 
+    removeXM2(str + 1);
 }
 
 int main() { 
-    // char arr[] = {'x','a','b','x','c','x'}; // output : abc
-    // char arr[] = {'x','x'}; // output: empty
-    removeX(arr);
-    for( int i = 0 ; arr[i]!='\0'; i++) cout << arr[i]; 
+    int T;
+    cin >> T;
+    while(T--) {
+        int size;
+        cin >> size;
+        char *arr = new char[size];
+        for(int i = 0 ; i < size; i++) {
+            cin >> arr[i];
+        }
+        removeXM2(arr);
+        for(int i = 0 ; arr[i] != '\0'; i++) {
+            cout << arr[i];
+        } cout << endl;
+    }
     return 0;
 }
