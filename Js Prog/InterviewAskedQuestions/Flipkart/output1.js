1e63a8590fda9d72131ac379ba6cb53c0f6b24e5
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-03-02 11:29:01 +0530

    Add note for the output explanation

--- Js Prog/InterviewAskedQuestions/Flipkart/output1.js
+++ Js Prog/InterviewAskedQuestions/Flipkart/output1.js
@@ -6,13 +6,15 @@
 var a = 3
 function ab() {
   var a = 2
-  setTimeout(() => console.log(this.a))
+  console.log(this)
+  setTimeout(() => console.log(this), 0)
 }
 c = ab()
  
 /*
     This will give 3 as output as the var a on line no 6 is present in global context not the var a which is inside the function
     so that's why it will give 3 as output 
+    Note: If you'll check using node it will give undefined but if you'll check in browser it will print 3
 */
 
 
