a73349c205f085271485687c82c225e9616293ad
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-03-30 01:21:38 +0530

    Update input object in map exercise.

--- Js Prog/MapFilterReduce/map.js
+++ Js Prog/MapFilterReduce/map.js
@@ -1,19 +1,22 @@
 /*
-    An exercise to clear the concept of HIGHER ORDER FUNCTIONS OF JS (MAP)
+    An exercise to clear the concept of 
+    HIGHER ORDER FUNCTIONS OF JS (MAP, FILTER, REDUCE)
+    
+    TOPIC: MAP
 */
 
 const arr = [
-    {name: "Elon", lastName:"Musk", age: 35},
-    {name: "Tim", lastName:"Cook", age: 50},
-    {name: "Bill", lastName:"Gates", age: 59},
-    {name: "Tony", lastName: "Stark", age: 35 },
-    {name: "Mark", lastName:"Zuckerberg", age: 35},
-  ]
+    { firstName: "Elon", lastName: "Musk", age: 35 },
+    { firstName: "Tim", lastName: "Cook", age: 50 },
+    { firstName: "Bill", lastName: "Gates", age: 59 },
+    { firstName: "Tony", lastName: "Stark", age: 35 },
+    { firstName: "Mark", lastName: "Zuckerberg", age: 35 },
+];
   
 /* 
     Problem: Wap a map function to get the names of the people along with their surnames output
     Eg : ["Elon Musk", "Tim Cook", ...]
 */
-  let fullNamesArr  = arr.map((value) => value.name +" "+ value.lastName)
+  let fullNamesArr  = arr.map((value) => value.firstName +" "+ value.lastName)
   
   console.log(fullNamesArr)
