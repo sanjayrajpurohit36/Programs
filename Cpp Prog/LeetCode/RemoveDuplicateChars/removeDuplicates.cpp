4aeae5e502fe7422c61b8838c6e791c52418901f
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-04-13 00:07:28 +0530

    Remove duplicate from string in O(n)

---
+++ Cpp Prog/LeetCode/RemoveDuplicateChars/removeDuplicates.cpp
@@ -0,0 +1,46 @@
+/*
+    Remove duplicate chars from the given in string with the below given time and space complexity
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<unordered_map>
+using namespace std;
+
+void removeDuplicates(string str) 
+{
+    unordered_map<char, bool> ump;
+    int rp = 0, wp = 0;
+    while(rp < str.length()) {
+
+        if(ump[str[rp]]) {
+                rp++;
+        } else {
+            ump[str[rp]] = true;
+            if(rp == wp) {
+                rp++;
+                wp++;
+            } else {
+                str[wp] = str[rp];
+                wp++;
+                rp++;
+            }
+        }
+    }
+    cout << str.substr(0, wp) << endl;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--) {
+        string str;
+        getline(cin, str);
+        removeDuplicates(str);
+    }
+    return 0;
+}
