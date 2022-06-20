038a0e3234cd0ce75e62911f43361fbb6081ccb9
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-06-21 01:41:47 +0530

    Create tree by taking input level wise

---
+++ Cpp Prog/CodingNinja/Trees/TreeLevelWiseInput/TreeUse.cpp
@@ -0,0 +1,72 @@
+/*
+    Take input level wise
+*/
+
+#include<iostream>
+#include<queue>
+#include "TreeNode.h"
+
+using namespace std;
+
+TreeNode<int>* takeInputLevelwise() {
+    int rootData;
+    cout << "Enter node data : ";
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    queue <TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+
+    // we need to do this process until the queue becomes empty
+    while(!pendingNodes.empty()) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+
+        int nodeChildren;
+        cout << "\n How many children TreeNode " << front->data << " will have : ";
+        cin >> nodeChildren;
+        /*
+            below code is for taking input of childnodes of the node which is present on the front of the queue
+        */
+        for(int i = 0; i < nodeChildren; i++) {
+            int childData;
+            cout << "Enter the " << i+1 << "th child of the node : ";
+            cin >> childData;
+
+            // we need to create these nodes dynamically so that in next loop the memory should not get de-allocated
+            TreeNode<int> *childNode = new TreeNode<int>(childData);
+            front->children.push_back(childNode);
+            pendingNodes.push(childNode);
+        }
+    }
+
+    return root;
+}
+
+void displayTree(TreeNode<int> *root) {
+    cout << root->data << " : ";
+    for (int i = 0; i < root->children.size();i++) {
+        cout << root->children[i]->data << " , ";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size();i++) {
+        displayTree(root->children[i]);
+    }
+}
+
+int main() {
+    // TreeNode<int>* root = new TreeNode<int>(1);
+    // TreeNode<int>* TreeNode1 = new TreeNode<int>(2);
+    // TreeNode<int>* TreeNode2 = new TreeNode<int>(3);
+
+    // Till this point only treee nodes are created not connected until we push the nodes in to the children of a node
+    // root->children.push_back(TreeNode1);
+    // root->children.push_back(TreeNode2);
+
+    TreeNode<int>* root = takeInputLevelwise();
+
+    // display the nodes
+    displayTree(root);
+
+    return 0;
+}
