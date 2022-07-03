f4eb8a9c7b8030f73a6706b6b8e0e11ecfcef30c
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-07-04 01:44:26 +0530

    Program update

--- Cpp Prog/CodingNinja/Trees/TreeLevelWiseInput/TreeUse.cpp
+++ Cpp Prog/CodingNinja/Trees/TreeLevelWiseInput/TreeUse.cpp
@@ -248,7 +248,11 @@ void replaceWithDepthValue(TreeNode<int>* root) {
  */
 bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) {
     // Write your code here
-	if(root1->children.size() != root2->children.size()) {
+    if(root1 == NULL && root2 == NULL) return true;
+
+    if((root1 != NULL && root2 == NULL) || (root1 == NULL && root2 != NULL)) return false;
+
+	if( (root1->data != root2->data)  || (root1->children.size() != root2->children.size())) {
         return false;
     }
 
@@ -256,14 +260,9 @@ bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) {
     	return areIdentical(root1->children[i], root2->children[i]);
     }
 
-    for(int i = 0; i < root1->children.size() ; i++) {
-        if(root1->children[i] != root2->children[i]) {
-            return false;
-        }
-    }
     return true;
-
 }
+
 int main() {
     // TreeNode<int>* root = new TreeNode<int>(1);
     // TreeNode<int>* TreeNode1 = new TreeNode<int>(2);
