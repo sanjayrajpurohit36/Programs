0142bd5da56c205e7bc1f973171116cd38d059ed
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-08-20 19:54:33 +0530

    Minor correction

--- Js Prog/output26.js
+++ Js Prog/output26.js
@@ -1,7 +1,7 @@
 /**
  * What will be the output of the below code
- * Concept: Call, Appli & Bind in Javascript
- * 
+ * Concept: Call, Apply & Bind in Javascript
+ *
  */
 
 let arr = [1, 2, 3, 4, 5];
