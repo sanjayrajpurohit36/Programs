f40a4d82cd42733ab598331432eec7b8f6824316
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-09-16 23:59:36 +0530

    minor change

--- Js Prog/promiserace.js
+++ Js Prog/promiserace.js
@@ -1,5 +1,4 @@
-                    /* It waits for only first promise to get resolved / reject
-                       and goes forward with its result. 
+                    /* It waits for only first promise to get resolved / reject and goes forward with its result.
                     */
 Promise.race([
   new Promise((resolve, reject) => setTimeout(() => resolve(1), 0)), // 1
