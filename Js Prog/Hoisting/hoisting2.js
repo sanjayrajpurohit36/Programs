b4a738919dbf34215865a25f612ae2699f881084
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2022-10-04 11:29:02 +0530

    Program for hoisting concept

---
+++ Js Prog/Hoisting/hoisting2.js
@@ -0,0 +1,17 @@
+/*  What will be the output of the following code
+    Concept: Hoisting, (function, block) scope variables, var, let
+*/
+
+
+var player = "messi"
+
+function displayPlayer() {
+    console.log(player)
+    if(player === "messi") {
+        let player = "ms";
+        console.log(player);
+    }
+    console.log(player)
+}
+
+displayPlayer();
