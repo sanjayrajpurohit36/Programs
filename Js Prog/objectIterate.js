a66cefafc91232968d1d401991d4fb2864c17607
Neha Karmakar <nehakarmakar88888.nk@gmail.com>
2022-10-12 20:25:18 +0530

    program comment update

--- Js Prog/objectIterate.js
+++ Js Prog/objectIterate.js
@@ -1,11 +1,17 @@
+/*
+  What will be the output of the given program
+*/
+
 const person = { name: "Sanjay", surname: "Raj" };
+
 for (const key in person) {
   console.log(key);
 }
 
 console.log(typeof typeof null);
-/* 
-typeof typeof null
-typeof Object
-ans: string
+
+/*
+  typeof typeof null
+  typeof Object
+  ans: string
 */
