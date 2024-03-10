49d5e9ba1178b133c537c7f9e2e7f2f970e18186
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-03-10 21:40:01 +0530

    minor update

--- Js Prog/promiseall.js
+++ Js Prog/promiseall.js
@@ -1,5 +1,11 @@
-Promise.all([
-  new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)), // 1
-  new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)), // 2
-  new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000))  // 3
-]).then((res) =>console.log(res));
+/*
+  What will be the output of the following code
+  Topic: Promise, Promise.all
+*/
+
+var p1 =  new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)) // 1
+var p2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)) // 2
+var p3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000))  // 3
+
+var promiseArray = [p1, p2, p3];
+Promise.all(promiseArray).then((res) => console.log(res));
