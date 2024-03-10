efc9dcd5025daf8ea8c7410e86df284efb9963a7
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-03-11 03:59:03 +0530

    Add reduce problem with solution

--- Js Prog/MapFilterReduce/reduce.js
+++ Js Prog/MapFilterReduce/reduce.js
@@ -8,16 +8,34 @@ const arr = [
     { firstName: "Bill", lastName: "Gates", age: 59 },
     { firstName: "Tony", lastName: "Stark", age: 35 },
     { firstName: "Mark", lastName: "Zuckerberg", age: 35 },
+    { firstName: "Peter", lastName: "Parker", age: 21}
 ];
 
-/* 
-    Problem: Return an object of the frequencies of the ages 
-    Ex: { 35: 3, 50:1, 59:1 } 
+/*
+    Problem: Return an object of the frequencies of the ages
+    Ex: { 35: 3, 50:1, 59:1 }
 */
 let uniqueAgeObj = arr.reduce((acc, currValue) => {
     if(acc[currValue.age])  { acc[currValue.age] =  acc[currValue.age] + 1; }
     else { acc[currValue.age] = 1; }
     return acc;
 }, {})
-  
+
 console.log(uniqueAgeObj);
+
+
+
+
+/*
+    Problem : Return the first name of all the people if their age is less than 26
+    Ex: [Peter]
+*/
+
+let person = arr.reduce((acc, curr) => {
+    if (curr?.age < 26) {
+        acc.push(curr?.firstName);
+    }
+    return acc;
+}, []);
+
+console.log(person);
