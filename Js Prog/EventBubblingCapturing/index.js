177691d056c7c262ea0af83f46b501046f62b389
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-03-23 15:52:50 +0530

    update code

--- Js Prog/EventBubblingCapturing/index.js
+++ Js Prog/EventBubblingCapturing/index.js
@@ -1,14 +1,14 @@
 // useCapture is the (flag / Key) used to control that whether Bubbling will happen or Trickling.
-
 let useCapture = true;
+
 document.querySelector("#grandparent").addEventListener("click", () => {
   console.log("Grandparent Clicked!!!");
 }, useCapture);
 
 document.querySelector("#parent").addEventListener("click", () => {
-  console.log("Parent Clicked!");
+  console.log("Parent Clicked!!");
 }, useCapture);
 
-document.querySelector("#child").addEventListener("click", () => {
+document.querySelector("#child1").addEventListener("click", () => {
   console.log("Child Clicked!");
 }, useCapture);
