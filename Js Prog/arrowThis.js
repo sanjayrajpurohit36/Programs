e6c0e6168b39f59961f12a5058f2e843053f5749
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-04-02 11:26:37 +0530

    update info

--- Js Prog/arrowThis.js
+++ Js Prog/arrowThis.js
@@ -3,15 +3,15 @@
 */
 
 const thisObj = {
-	thisFunc: function () {
-        /*
-            Value of "this" inside the function according to it's calling on line number 17
-            is thisObj.
+  thisFunc: function () {
+    /*
+            Value of "this" object inside the normal function is determined according to the way it's called 
+            on line number 17 i.e thisObj. (MIP) is used
         */
-		const arrowFunc = () => console.log(this);
-		arrowFunc();
-    },
-    name: "Max"
+    const arrowFunc = () => console.log(this);
+    arrowFunc();
+  },
+  name: "Max",
 };
 
 thisObj.thisFunc();
@@ -21,9 +21,10 @@ console.log(thisObj);
     Key Point: As the FAT arrow function don't have their own "this" that's why they
     reference / inherit "this" from their ancestors or parent.
 
-    Also, here MIP is used Method Invocation Pattern due to which the this inside the
+    Also, here MIP is used Method Invocation Pattern due to which the "this" object inside the
     FAT arrow function will refer this as whatever is written before "." operator.
     so,
+    
     Output: thisObj which is
     { thisFunc : [Function: thisFunc], name: 'Max' }
 */
