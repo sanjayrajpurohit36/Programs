2de6e68b672f17c93c5582ce760e1c1de2584a6c
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-04-03 17:21:43 +0530

    Updated comments

--- Js Prog/thisFunctions2.js
+++ Js Prog/thisFunctions2.js
@@ -1,18 +1,31 @@
 /*
     What will be the output of the following code
     Concept: normal function / arrow function, this
+    Note: Run this code in browser
 */
 
-var name = "test name"
+var superHero = "Iron Man";
 var testObj = {
-    name: "Max",
-    printName: function () {
-        console.log(this.name)
-        const print = () =>  console.log(this);
-        print();
-    },
-    printName1: () => console.log(this.name)
-}
+  name: "Max",
+  printName: function () {
+    console.log("First: ", this.name);
+  },
+  printSuperHero: () => console.log("Second: ", this.superHero),
+};
 
-testObj.printName()
-testObj.printName1()
+testObj.printName();
+testObj.printSuperHero();
+
+/**
+    Output: 
+    First: Max
+    Second: Iron Man
+
+    when we're calling the testObj.printName(); then it is MIP (Method invocation pattern) in which the value of "this" 
+    inside the function will the the object which is mentioned before . operator, thats why it will print the Max as output
+    as inside the printName function "this" is referring to thisObj.
+
+    But the same is not the case with arrow functions as we know that arrow functions don't have their own this so they
+    inherit the value to "this" form their parent / or from their parent's lexical scope which in this case will point to the
+    window object, hence it will have the access to the variable superHero hence it will print "Iron Man"
+*/
