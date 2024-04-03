d7a93c9980e7312d3f80d547dfaba2a4d2515c87
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-04-03 17:03:24 +0530

    update comments

--- Js Prog/thisFunctions.js
+++ Js Prog/thisFunctions.js
@@ -7,11 +7,11 @@
 //   console.log(this); // window / global object
 // }
 
-// FAT Arrow Function
+// FAT Arrow Function ES6 feature
 const test2 = () => {
   console.log(this);
   /* 
-    Note: Check in browser scripts
+    Note: Run this code in browser scripts
     window / global object as the FAT arrow functions don't have their own this, 
     so they access the value of "this" object from their parent's scope, which in this case is 
     global object / window object
