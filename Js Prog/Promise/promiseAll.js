e066071e52e8cceae0b29cce478413989ddb5820
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-04-04 08:17:55 +0530

    update comments

--- Js Prog/Promise/promiseAll.js
+++ Js Prog/Promise/promiseAll.js
@@ -1,11 +1,12 @@
 /*
   What will be the output of the following code
   Topic: Promise, Promise.all
+  Used for parallel api calls, it will take the 3 seconds to resolve the result
 */
 
-var p1 =  new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)) // 1
-var p2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)) // 2
-var p3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000))  // 3
+var p1 = new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)); // 1
+var p2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)); // 2
+var p3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000)); // 3
 
 var promiseArray = [p1, p2, p3];
 Promise.all(promiseArray).then((res) => console.log(res));
