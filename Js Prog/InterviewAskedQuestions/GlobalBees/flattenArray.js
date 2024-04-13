15118a3090b67a4f29ecf64cc84fc22e95d5c537
Sanjay Rajpurohit <sanjayrajpurohit36@gmail.com>
2024-04-13 23:29:15 +0530

    Add new approach for flattenArray program

--- Js Prog/InterviewAskedQuestions/GlobalBees/flattenArray.js
+++ Js Prog/InterviewAskedQuestions/GlobalBees/flattenArray.js
@@ -10,14 +10,14 @@
 */
 
 function flattenArray(arr = [], res = []) {
-	for (let i = 0; i < arr.length; i++) {
-		if (Array.isArray(arr[i])) {
-			flattenArray(arr[i], res);
-		} else {
-			res.push(arr[i]);
-		}
-	}
-	return res;
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            flattenArray(arr[i], res);
+        } else {
+            res.push(arr[i]);
+        }
+    }
+    return res;
 }
 // Test cases
 var t1 = [[1, 2, 3], 4, 5, [6, 7, [8, 9]]];
@@ -27,3 +27,22 @@ var t3 = [1, 2, [3, [4], 5], [6, 7], 8, [10]];
 console.log(flattenArray(t1));
 console.log(flattenArray(t2));
 console.log(flattenArray(t3));
+
+// Without using the res array in the function params
+
+function flattenArrayApproach(arr) {
+    let res = [];
+    for (let element of arr) {
+        if (Array.isArray(element)) {
+            let rCallAns = flattenArrayApproach(element);
+            res = [...res, ...rCallAns];
+        } else {
+            res.push(element);
+        }
+    }
+    return res;
+}
+
+console.log(flattenArrayApproach(t1));
+console.log(flattenArrayApproach(t2));
+console.log(flattenArrayApproach(t3));
